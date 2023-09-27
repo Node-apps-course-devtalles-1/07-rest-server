959fe71e559ad21ba44ffad73d49f7667812b540
henryGustav <henr_gustavo18@hotmail.com>
2023-09-27 13:29:22 -0500

    test process env

--- database/config.js
+++ database/config.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 export const dbConnection = async () => {
   try {
+    console.log('MONGO:',process.env.MONGODB_ATLAS)
     await mongoose.connect(process.env.MONGODB_ATLAS)
     console.log('Data base on line')
   } catch (error) {
